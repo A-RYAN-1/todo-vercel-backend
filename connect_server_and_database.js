e8843af5ba0845e21bc47d0f732f4875a93eeb29
Aryan Padmakar Deshmukh <aryan0deshmukh@gmail.com>
2025-07-05 18:54:06 +0530

    Update connect_server_and_database.js

--- connect_server_and_database.js
+++ connect_server_and_database.js
@@ -1,8 +1,11 @@
+// server/connect_server_to_database.js
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
